6f3132530970eb00e9eb32cc4a3405636ca5a8ce
Alexsky347 <ledain.alexis@gmail.com>
2023-10-26 23:54:10 +0200

    again

--- init.sh
+++ init.sh
@@ -48,11 +48,11 @@ echo REACT_APP_BACKEND_URL=http://localhost/api >> frontend/.env
 docker compose up -d frontend
 docker compose up certbot
 docker compose down
- 
+
 # # some configurations for let's encrypt
 curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
 openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
- 
+
 # # Phase 2
 crontab ./etc/crontab
 docker compose up -d
